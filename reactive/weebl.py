c1b56b0d82606d776059bf6a30d596e06166f908
Jason Hobbs <jason.hobbs@canonical.com>
2015-12-15 19:09:29 -0600

    Fix static file collection.

--- reactive/weebl.py
+++ reactive/weebl.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3.5
+#!/usr/bin/env python3
 import os
 import errno
 import yaml
@@ -20,6 +20,9 @@ from charms.reactive import (
     set_state,
     remove_state,
     )
+
+
+JSLIBS_DIR = '/var/lib/weebl/static'
 
 
 config = hookenv.config()
@@ -47,19 +50,10 @@ def check_admin_pass():
     else:
         remove_state('admin-pass')
 
-JSLIBS_DIR = 'weebl/oilserver/static'
-
 
 def install_pip_deps():
     pip_packages = ["django-tastypie-swagger", "django-extensions"]
     command = ["python3.4", "-m", "pip", "install"] + pip_packages
-    check_call(command)
-
-
-def install_npm_deps():
-    mkdir_p(JSLIBS_DIR)
-    npm_packages = ["d3", "nvd3", "angular-nvd3"]
-    command = ["npm", "install", "--prefix", JSLIBS_DIR] + npm_packages
     check_call(command)
 
 
@@ -79,18 +73,34 @@ def install_weebl_deb():
     apt_install(['python3-weebl'])
 
 
+def collect_static():
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'weebl.settings'
+    check_call(['django-admin', 'collectstatic', '--noinput'])
+
+
+def install_npm_deps():
+    mkdir_p(JSLIBS_DIR)
+    npm_packages = ["d3", "nvd3", "angular-nvd3"]
+    command = ["npm", "install", "--prefix", JSLIBS_DIR] + npm_packages
+    check_call(command)
+
+
 @when('nginx.available')
 def install_weebl(*args, **kwargs):
     hookenv.log('Installing weebl!')
     install_weebl_deb()
     hookenv.log('Installing pip packages!')
     install_pip_deps()
+    hookenv.log('Collecting static files!')
+    collect_static()
     hookenv.log('Installing npm packages!')
     install_npm_deps()
     setup_weebl_gunicorn_service()
+    check_call(['service', 'weebl-gunicorn', 'start'])
+    check_call(['service', 'nginx', 'restart'])
 
 
-def render_db_config(pgsql):
+def render_config(pgsql):
     db_settings = {
         'host':  pgsql.host(),
         'port': pgsql.port(),
@@ -98,14 +108,18 @@ def render_db_config(pgsql):
         'user': pgsql.user(),
         'password': pgsql.password(),
     }
+    config = {
+        'database': db_settings,
+        'static_root': '/var/lib/weebl/static',
+    }
     mkdir_p('/etc/weebl/')
-    with open('/etc/weebl/weebl-db.yaml', 'w') as weebl_db:
-        weebl_db.write(yaml.dump(db_settings))
+    with open('/etc/weebl/weebl.yaml', 'w') as weebl_db:
+        weebl_db.write(yaml.dump(config))
 
 
 @when('database.database.available')
 def setup_database(pgsql):
     hookenv.log('Configuring weebl db!')
     hookenv.status_set('maintenance', 'weebl is connecting to pgsql!')
-    render_db_config(pgsql)
+    render_config(pgsql)
     hookenv.status_set('active', 'Ready')
