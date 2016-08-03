512e4ff770757050f481828bcc4629fdb12d10b1
Greg Lutostanski <gregory.lutostanski@canonical.com>
2016-08-03 16:17:55 -0500

    update upload script

--- upload.sh
+++ upload.sh
@@ -1,6 +1,13 @@
-#!/bin/bash
+#!/bin/bash -ex
 # Should be run from top of source tree
 
+rm -fr .temp
 [[ -n "$(bzr status | grep -v shelve)" ]] && echo "Repo not clean" && exit 1
-mkdir temp
-charm build -o temp
+mkdir -p .temp/builds
+bzr checkout lp:~oil-ci/oil-ci/charm-weebl-BUILT .temp/builds/weebl
+charm build -o .temp
+rm -fr .temp/builds/weebl/.temp
+log=$(bzr log -r-1 --line)
+cd .temp/builds/weebl
+bzr commit -am "$log"
+rm -fr .temp
