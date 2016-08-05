ec65119a321400a75f77068440d07fae80d55c8e
Darren Hoyland <darren.hoyland@canonical.com>
2016-08-05 10:41:49 +0100

    also push to charmstore

--- upload.sh
+++ upload.sh
@@ -2,7 +2,11 @@
 # Should be run from top of source tree
 
 [[ -n "$(bzr status | grep -v shelve)" ]] && echo "Repo not clean" && exit 1
+userstr=$(charm whoami | grep "User")
+stringarray=($userstr)
+echo Logged in as ${stringarray[1]}
 charm build
+charm push builds/weebl/ weebl
 bzr checkout lp:~oil-ci/oil-ci/charm-weebl-BUILT builds/weebl-built
 cp -R builds/weebl-built/.bzr builds/weebl/
 rm -fr builds/weebl-built/builds
