bcaa18280c993a18688693feb8c82c5ce775d957
Darren Hoyland <darren.hoyland@canonical.com>
2016-09-27 17:49:22 +0100

    push to ~oil-charms group

--- upload.sh
+++ upload.sh
@@ -3,7 +3,6 @@
 
 [[ -n "$(bzr status | grep -v shelve)" ]] && echo "Repo not clean" && exit 1
 
-
 function tidy_up () (
     cd $startdir
     rm -fr builds deps
@@ -17,20 +16,35 @@ userstringarray=($userstr)
 USER=${userstringarray[1]}
 echo Logged in as $USER
 
-charm build
+charm build -o .
 
-output=$(charm push builds/weebl/ weebl)
+output=$(charm push ./builds/weebl/ cs:~oil-charms/weebl)
 charmstringarray=($output)
 CHARM=${charmstringarray[1]}
 
 while [ ! $# -eq 0 ]
 do
     case "$1" in
-        --publish | -p)
-            charm publish $CHARM --channel stable
+        --publish_development | -d)
+            pub_output=$(charm publish ${CHARM} --channel development)
+            ;;
+        --publish_stable | -s)
+            pub_output=$(charm publish ${CHARM} --channel stable)
+            ;;
+        --*)
+            pub_output=''
+            ;;
     esac
     shift
 done
+
+pubarray=($pub_output)
+CHANNEL=${pubarray[3]}
+if [[ -z  $CHANNEL  ]]; then
+    MSG="This charm has not been published."
+else
+    MSG="This charm has been published to ${CHANNEL}"
+fi
 
 bzr checkout lp:~oil-ci/oil-ci/charm-weebl-BUILT builds/weebl-built
 cp -R builds/weebl-built/.bzr builds/weebl/
@@ -41,3 +55,4 @@ bzr add
 bzr commit -m "$log"
 cd $startdir
 tidy_up
+echo $MSG
