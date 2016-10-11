c533bedd2fb28e440af9ea33b0a979089070b067
Darren Hoyland <darren.hoyland@canonical.com>
2016-10-11 11:16:18 +0100

    tmp

--- reactive/weebl.py
+++ reactive/weebl.py
@@ -98,7 +98,10 @@ def install_weebl(*args, **kwargs):
     setup_weebl_gunicorn_service()
     utils.cmd_service('start', 'weebl-gunicorn', hookenv)
     utils.cmd_service('restart', 'nginx', hookenv)
-    load_fixtures()
+    try:
+        load_fixtures()
+    except:
+        pass
     setup_weebl_site(config['username'])
     utils.fix_bundle_dir_permissions()
     if not weebl_ready:
