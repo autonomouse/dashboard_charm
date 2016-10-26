9f6beff4b25ef8f6e5d55ec5585e8f223513489e
Darren Hoyland <darren.hoyland@canonical.com>
2016-10-26 17:32:20 +0100

    responses to review comments

--- get_tarballs.py
+++ get_tarballs.py
@@ -9,25 +9,39 @@ from lib.charms.layer.weebl import constants
 from subprocess import check_output, check_call
 
 
-def update_debs():
-    cache = apt.cache.Cache()
+def install_debs(install_me):
     try:
         cache.update()
         cache.open()
     except LockFailedException:
         sys.exit("\nPlease run again as sudo\n")
 
+    for pkg_name in install_me:
+        pkg = cache[pkg_name]
+        pkg.mark_install()
+        cache.commit()
+
+
+def update_debs_if_necessary():
+    cache = apt.cache.Cache()
+    install_me = []
     for pkg_name in constants.DEB_PKGS:
         pkg = cache[pkg_name]
         if not pkg.is_installed:
-            pkg.mark_install()
-            cache.commit()
+            install_me.append(pkg)
+    if install_me:
+        install_debs(install_me)
+
+
+def chown(path):
+    sudo_id = os.environ.get('SUDO_ID', 1000)
+    sudo_gid = os.environ.get('SUDO_GID', 1000)
+    check_call("chown -R {}:{} {}".format(
+        sudo_id, sudo_gid, path), shell=True)
 
 
 def custom_update(directory, pkgs, cmd):
     original_wd = os.getcwd()
-    sudo_id = os.environ.get('SUDO_ID', 1000)
-    sudo_gid = os.environ.get('SUDO_GID', 1000)
     path = os.path.abspath(directory)
     try:
         shutil.rmtree(path)
@@ -40,8 +54,7 @@ def custom_update(directory, pkgs, cmd):
             check_output(cmd.format(pkg), shell=True)
     finally:
         os.chdir(original_wd)
-        check_call("chown -R {}:{} {}".format(
-            sudo_id, sudo_gid, path), shell=True)
+        chown(path)
 
 
 def update_pip():
@@ -50,10 +63,11 @@ def update_pip():
 
 def update_npm():
     custom_update(constants.NPMDIR, constants.NPM_PKGS, "npm pack {}")
+    chown("~/.npm") # So don't always need to run with sudo
 
 
 def main():
-    update_debs()
+    update_debs_if_necessary()
     update_pip()
     update_npm()
 
