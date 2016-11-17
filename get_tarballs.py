4d220c44985a9ab4832d53aa07a5f9d51935a2af
Darren Hoyland <darren.hoyland@canonical.com>
2016-11-17 16:58:22 +0000

    Stopped get_tarballs from deleting yamls now that they're stored in the pkg folders

--- get_tarballs.py
+++ get_tarballs.py
@@ -5,6 +5,7 @@ import sys
 import apt
 import yaml
 import shutil
+import tempfile
 from subprocess import check_call
 from apt.cache import LockFailedException
 
@@ -25,7 +26,6 @@ def install_debs(requires_installation, cache):
         cache.open()
     except LockFailedException:
         sys.exit("\nPlease run again as sudo\n")
-
     for pkg_name in requires_installation:
         pkg = cache[pkg_name]
         pkg.mark_install()
@@ -43,31 +43,37 @@ def update_debs_if_necessary():
         install_debs(requires_installation, cache)
 
 
-def generate_local_pkgs(directory, pkgs, cmd):
+def generate_local_pkgs(directory, pkgs, cmd, yaml_file):
     original_wd = os.getcwd()
     path = os.path.abspath(directory)
-    try:
-        shutil.rmtree(path)
-    except FileNotFoundError:
-        pass
-    os.mkdir(path)
-    try:
-        os.chdir(path)
-        for pkg in pkgs:
-            check_call(cmd.format(pkg), shell=True)
-    finally:
-        os.chdir(original_wd)
-        shutil.chown(path=path, user=os.environ.get('USER'))
+    with tempfile.TemporaryDirectory() as tmp:
+        try:
+            shutil.move(yaml_file, tmp)
+            shutil.rmtree(path)
+        except FileNotFoundError:
+            pass
+        os.mkdir(path)
+        try:
+            os.chdir(path)
+            for pkg in pkgs:
+                check_call(cmd.format(pkg), shell=True)
+        finally:
+            os.chdir(original_wd)
+            shutil.move(os.path.join(
+                tmp, os.path.basename(yaml_file)), os.path.dirname(yaml_file))
+            shutil.chown(path=path, user=os.environ.get('USER'))
 
 
 def generate_pip_wheels():
-    pip_pkgs = get_pkgs_from_list("./wheels/wheels.yaml")
-    generate_local_pkgs("./wheels/", pip_pkgs, "pip3 wheel {}")
+    yaml_file = "./wheels/wheels.yaml"
+    pip_pkgs = get_pkgs_from_list(yaml_file)
+    generate_local_pkgs("./wheels/", pip_pkgs, "pip3 wheel {}", yaml_file)
 
 
 def generate_npm_packs():
-    npm_pkgs = get_pkgs_from_list("./npms/npms.yaml")
-    generate_local_pkgs("./npms/", npm_pkgs, "npm pack {}")
+    yaml_file = "./npms/npms.yaml"
+    npm_pkgs = get_pkgs_from_list(yaml_file)
+    generate_local_pkgs("./npms/", npm_pkgs, "npm pack {}", yaml_file)
 
 
 def main():
